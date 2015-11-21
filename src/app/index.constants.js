274d6c42cab7a719a05bc34029e2fd92cc7512ae
Igor Bondarenko <noscream2@gmail.com>
2015-11-21 12:37:56 +0800

    Initial commit

---
+++ src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('viewSound')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
