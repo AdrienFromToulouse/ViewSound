a5247714e0eec765750b6ae6c0c48d8d9608e400
Paul Buramensky <paul.buramensky@bandlab.com>
2015-11-21 18:22:22 +0800

    Add letter notes

--- src/app/components/imageToSong/imageToMidi.service.js
+++ src/app/components/imageToSong/imageToMidi.service.js
@@ -20,7 +20,8 @@
         sounds = [],
         currentTime = 0,
         pixelsAmount = 0,
-        totalDuration = 5; // seconds
+        totalDuration = 5, // seconds
+        noteStrings = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'];
 
       for (var swatch in swatches) {
         if (swatches.hasOwnProperty(swatch) && swatches[swatch]) {
@@ -38,12 +39,15 @@
             lightness = colorsObj.hsl[2],
             population = colorsObj.population,
             duration = (population / pixelsAmount) * totalDuration,
+            octave = Math.ceil(lightness * 9) + 1,
+            note = Math.ceil(hue * 11) * octave,
             sound = {
-              note: Math.ceil(hue * 11) * Math.ceil(lightness * 9) + 12,
+              note: note,
               velocity: Math.min(Math.floor(saturation * 127 * 2), 127),
               timeStart: currentTime,
               timeEnd: currentTime + duration,
-              backgroundColor: colorsObj.getHex()
+              backgroundColor: colorsObj.getHex(),
+              letterNote: noteStrings[note % 12] + octave
             };
           sounds.push(sound);
 
